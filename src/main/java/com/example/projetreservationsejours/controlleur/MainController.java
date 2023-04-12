3c5849f292c71a469345dcd1be8b421ffab2934f
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-04-12 17:28:32 +0200

    Feat(Recherche) : Ajout de la recherche

--- src/main/java/com/example/projetreservationsejours/controlleur/MainController.java
+++ src/main/java/com/example/projetreservationsejours/controlleur/MainController.java
@@ -5,11 +5,14 @@ import com.example.projetreservationsejours.Application;
 import com.example.projetreservationsejours.modele.AllLocation;
 import com.example.projetreservationsejours.modele.AllUser;
 import com.example.projetreservationsejours.modele.Location;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -19,7 +22,11 @@ import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class MainController implements Initializable {
 
@@ -27,6 +34,12 @@ public class MainController implements Initializable {
 
     @FXML
     private VBox cardContainer;
+
+    @FXML
+    private DatePicker dateDebut;
+
+    @FXML
+    private DatePicker dateFin;
 
     @FXML
     private Button connexion;
@@ -44,7 +57,13 @@ public class MainController implements Initializable {
     private ImageView shopping_cart;
 
     @FXML
+    private TextField searchTextField;
+
+    @FXML
     private ImageView user;
+
+    @FXML
+    private Text nbLocation;
 
     @FXML
     private Text userName;
@@ -89,6 +108,99 @@ public class MainController implements Initializable {
         }
     }
 
+
+    /**
+     * For all the locations available:
+     *  - check if searchTextField, dateDebut and dateFin is in the resource file
+     *      -> Not null: display elements
+     *      -> Null: display error
+     *  - possibilities to search on field or more
+     * */
+    @FXML
+    void searchBar(ActionEvent event) {
+        AllLocation allLocation = new AllLocation();
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            allLocation.loadData("locations.csv", searchTextField.getText(), dateDebut.getValue().format(formatter), dateFin.getValue().format(formatter));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        allLocation.displayLocationList();
+
+        if(this.isUserConnected()) {
+            userName.setText(application.userConnected.getNom());
+        }
+
+        cardContainer.getChildren().clear();
+        int cpt = 0;
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.BASELINE_CENTER);
+
+        if(searchTextField.getText().equals("")) {
+            try {
+                allLocation.loadData("locations.csv");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            allLocation.displayLocationList();
+
+            if(this.isUserConnected()) {
+                userName.setText(application.userConnected.getNom());
+            }
+            for (Location card : allLocation.getLocationList()) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("cardTemplate.fxml"));
+                    AnchorPane cardNode = loader.load();
+                    CardTemplateControlleur cardController = loader.getController();
+                    cardController.setCard(card);
+                    if (cpt < 3) {
+                        hBox.getChildren().add(cardNode);
+                        cpt++;
+                    } else {
+                        cardContainer.getChildren().add(hBox);
+                        cpt = 0;
+                        hBox = new HBox();
+                        hBox.setAlignment(Pos.BASELINE_CENTER);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        else if (allLocation.getLocationList().isEmpty()) {
+            hBox.getChildren().add(new Text("La recherche n'a pas aboutie"));
+            cardContainer.getChildren().add(hBox);
+        }
+        else {
+            for (Location card : allLocation.getLocationList()) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("cardTemplate.fxml"));
+                    AnchorPane cardNode = loader.load();
+                    CardTemplateControlleur cardController = loader.getController();
+                    cardController.setCard(card);
+                    if (allLocation.getLocationList().size() < 3) {
+                        hBox.getChildren().add(cardNode);
+                        cardContainer.getChildren().add(hBox);
+                    } else if (cpt < 3) {
+                        hBox.getChildren().add(cardNode);
+                        cpt++;
+                    } else {
+                        cardContainer.getChildren().add(hBox);
+                        cpt = 0;
+                        hBox = new HBox();
+                        hBox.setAlignment(Pos.BASELINE_CENTER);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Go back to home windows
+     * */
     @FXML
     void backHome(MouseEvent event) throws IOException {
         application.fenetreControlleur.changerDeFenetre("Accueil.fxml");
@@ -107,6 +219,9 @@ public class MainController implements Initializable {
         changeHeaderVisibility();
     }
 
+    /**
+     * Reset the status of the user connected
+     * */
     @FXML
     void logout(MouseEvent event) {
         application.userConnected = null;
@@ -114,18 +229,32 @@ public class MainController implements Initializable {
         changeHeaderVisibility();
     }
 
+    /**
+     * If user is connected, display his personal header
+     * Else display sign-in and sign-up buttons
+     * */
     public void changeHeaderVisibility() {
+        /*
+        * Only visible when the user is connected
+        * */
         home.setVisible(isUserConnected());
         shopping_cart.setVisible(isUserConnected());
         user.setVisible(isUserConnected());
         userName.setVisible(isUserConnected());
         logout.setVisible(isUserConnected());
+        nbLocation.setVisible(isUserConnected());
 
-        //Show
+        /*
+        * Only visible when the user is disconnected
+         */
         connexion.setVisible(!isUserConnected());
         inscription.setVisible(!isUserConnected());
     }
 
+    /**
+     * Check if the user is connected
+     * @return boolean
+     * */
     public boolean isUserConnected() {
         return application.userConnected != null;
     }
--- src/main/java/com/example/projetreservationsejours/modele/AllLocation.java
+++ src/main/java/com/example/projetreservationsejours/modele/AllLocation.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,9 +28,41 @@ public class AllLocation {
         }
     }
 
+    public void loadData(String filename, String country) throws IOException {
+        String pathRessources = "\\src\\main\\resources\\com\\example\\projetreservationsejours\\ressources\\";
+        Path path = Paths.get(System.getProperty("user.dir")+ pathRessources + filename);
+        try (BufferedReader reader = new BufferedReader(new FileReader(path.toRealPath().toFile()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Location location = Location.fromCsv(line);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                if(location.getLocation().equals(country)) {
+                    locationList.add(location);
+                }
+            }
+        }
+    }
+
+    public void loadData(String filename, String country, String dateDebut, String dateFin) throws IOException {
+        String pathRessources = "\\src\\main\\resources\\com\\example\\projetreservationsejours\\ressources\\";
+        Path path = Paths.get(System.getProperty("user.dir")+ pathRessources + filename);
+        try (BufferedReader reader = new BufferedReader(new FileReader(path.toRealPath().toFile()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Location location = Location.fromCsv(line);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                if(location.getLocation().equals(country) && location.getStartDate().format(formatter).equals(dateDebut) && location.getEndDate().format(formatter).equals(dateFin)) {
+                    locationList.add(location);
+                }
+            }
+        }
+    }
+
     public List<Location> getLocationList() {
         return locationList;
     }
+
+    public void setLocationList(List<Location> locationList) { this.locationList = locationList;}
 
     public void displayLocationList() {
         System.out.println("Informations {");
