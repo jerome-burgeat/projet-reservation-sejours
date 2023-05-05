8c972a5176b1aecc7703471376b07e9582234bd0
Marie Zephir <mz@efidem.com>
2023-05-05 14:39:24 +0200

    "The styling of the incription page and modification if the logos of the main page"

--- src/main/java/com/example/projetreservationsejours/controlleur/PageInscriptionControlleur.java
+++ src/main/java/com/example/projetreservationsejours/controlleur/PageInscriptionControlleur.java
@@ -79,11 +79,6 @@ public class PageInscriptionControlleur extends Preloader implements Initializab
 
     private User user = new User();
 
-    public void setButton(Button button) {
-            button.setBackground(new Background(new BackgroundFill(Color.WHEAT, null, null)));
-            button.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-    }
-
     @FXML
     void checkHote(ActionEvent event) {
 
@@ -183,6 +178,12 @@ public class PageInscriptionControlleur extends Preloader implements Initializab
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        prenom.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #D3D3D3");
+        nom.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #D3D3D3");
+        nomUtilisateur.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #D3D3D3");
+        email.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #D3D3D3");
+        motDePasse.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #D3D3D3");
+        pageInscriptionStage.setStyle("-fx-background-color: #FFFFFF;");
+        boutonInscrire.setStyle("-fx-background-color: #800020; -fx-text-fill:#FFFFFF; -fx-border-radius: 30;-fx-background-radius: 30;-fx-border-color: #FFFFFF; -fx-arc-width: 30");
     }
 }
